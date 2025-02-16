93295d2436df72b7601800194a40e7d0f1015788
ilikesunday <dhanushnatra7@gmail.com>
2025-02-16 11:43:34 -0500

    changed settings using hive

--- lib/screens/Settings/Settings_Screen.dart
+++ lib/screens/Settings/Settings_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,6 +9,39 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool _isDarkMode = false;
+  bool _isEndlessPlayback = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  void _loadSettings() async {
+    var box = await Hive.openBox('settings');
+    setState(() {
+      _isDarkMode = box.get('darkMode', defaultValue: false);
+      _isEndlessPlayback = box.get('endlessPlayback', defaultValue: false);
+    });
+  }
+
+  void _toggleDarkMode(bool value) async {
+    var box = await Hive.openBox('settings');
+    setState(() {
+      _isDarkMode = value;
+      box.put('darkMode', value);
+    });
+  }
+
+  void _toggleEndlessPlayback(bool value) async {
+    var box = await Hive.openBox('settings');
+    setState(() {
+      _isEndlessPlayback = value;
+      box.put('endlessPlayback', value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +52,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   color: Colors.green, fontFamily: 'Modak', fontSize: 35)),
         ),
       ),
-      body: Center(
-        child: Text('Settings'),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text("DarkMode"),
+            trailing: Switch(
+              value: _isDarkMode,
+              onChanged: _toggleDarkMode,
+            ),
+          ),
+          ListTile(
+            title: Text("Endless playback"),
+            trailing: Switch(
+                value: _isEndlessPlayback, onChanged: _toggleEndlessPlayback),
+          )
+        ],
       ),
     );
   }
